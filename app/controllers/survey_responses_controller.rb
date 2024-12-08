b1cd96add6015fcf2c8bace1f672209068b075d5
Marshall Scott <bacchist@gmail.com>
2024-12-08 15:49:40 -0500

    add flowbite

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -1,20 +1,47 @@
 class SurveyResponsesController < ApplicationController
+  before_action :load_survey_response, except: [:new, :completion]
+  
+  STEPS = ['contact', 'values', 'issues', 'involvement', 'additional']
+
   def new
-    @survey_response = SurveyResponse.new
+    session[:survey_response] = {}
+    redirect_to survey_step_path('contact')
   end
 
-  def create
-    @survey_response = SurveyResponse.new(survey_response_params)
-    if @survey_response.save
-      redirect_to root_path, notice: "Thank you for your response!"
+  def show_step
+    @step = params[:step]
+    @survey_response = SurveyResponse.new(session[:survey_response])
+    @current_step_index = STEPS.index(@step)
+    
+    render "survey_responses/steps/#{@step}"
+  end
+
+  def update_step
+    @step = params[:step]
+    session[:survey_response] = session[:survey_response].merge(survey_params)
+    @current_step_index = STEPS.index(@step)
+    
+    if @step == STEPS.last
+      @survey_response = SurveyResponse.new(session[:survey_response])
+      if @survey_response.save
+        session.delete(:survey_response)
+        redirect_to completion_path(status: :success)
+      else
+        redirect_to completion_path(status: :error), alert: @survey_response.errors.full_messages.join(", ")
+      end
     else
-      render :new, status: :unprocessable_entity
+      next_step = STEPS[@current_step_index + 1]
+      redirect_to survey_step_path(next_step)
     end
+  end
+
+  def completion
+    @status = params[:status]&.to_sym || :success
   end
 
   private
 
-  def survey_response_params
+  def survey_params
     params.require(:survey_response).permit(
       :email, :discord_handle,
       :interest, :values, :goals, :top_issues, :meeting_preferences,
@@ -22,5 +49,10 @@ class SurveyResponsesController < ApplicationController
       :contact_method, :political_background, :demographics,
       :community_impact, :fundraising, :networking, :feedback
     )
+  end
+
+  def load_survey_response
+    return if session[:survey_response]
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'survey_responses#new'
-  resources :survey_responses, only: [:new, :create]
+  
+  get 'survey/:step', to: 'survey_responses#show_step', as: :survey_step
+  patch 'survey/:step', to: 'survey_responses#update_step', as: :update_survey_step
+  get 'completion', to: 'survey_responses#completion', as: :completion
 end
